735508315e2bf1eb8cad382de454f2ed7594a0c9
BryttLucas <workingB@fuse.net>
2019-11-18 19:49:55 -0500

    update to PRS

--- PrsServerProject/src/main/java/com/maxtrain/bootcamp/prs/request/RequestController.java
+++ PrsServerProject/src/main/java/com/maxtrain/bootcamp/prs/request/RequestController.java
@@ -3,7 +3,6 @@ package com.maxtrain.bootcamp.prs.request;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +12,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.maxtrain.bootcamp.prs.util.JsonResponse;
 
 @CrossOrigin
-@Controller
+@RestController
 @RequestMapping(path="/request")
 public class RequestController {
 
@@ -27,36 +27,39 @@ public class RequestController {
 	@GetMapping()
  	public @ResponseBody JsonResponse getAll( ) {
 		Iterable<Request> request = requestRepo.findAll();
-		return JsonResponse.getInstance(request);
-	}
+		    return JsonResponse.getInstance(request);
+	   }
 	@GetMapping("/{id}")
 	public @ResponseBody JsonResponse get(@PathVariable Integer id) {
-		try {
-			Iterable<Request> request = requestRepo.findAll();
+	
+			Optional<Request> request = requestRepo.findById(id);
 			if(!request.isPresent()) {
 				return JsonResponse.getInstance("Request not found");
-		}
+	   }
 			return JsonResponse.getInstance(request.get());
-   }catch(Exception ex) {
-	   return JsonResponse.getInstance(ex.getMessage());
        }
-   }private JsonResponse save(Request request) {
+	private JsonResponse save(Request request) {
 		try {
 			return JsonResponse.getInstance(requestRepo.save(request));
 		}catch  (Exception ex) {
 			return JsonResponse.getInstance(ex.getMessage());
 		}
 		}
-		@PostMapping()   
-		public @ResponseBody JsonResponse Insert(@RequestBody Request request) {
+	@PostMapping()
+	public JsonResponse add(@RequestBody Request request) {
+		try {
 			return save(request);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return JsonResponse.getInstance(e);
 		}
+	    }	
 		@PutMapping("/{id}")
-		public @ResponseBody JsonResponse update(@RequestBody Request request, @PathVariable Integer id) {
+	public @ResponseBody JsonResponse update(@RequestBody Request request, @PathVariable Integer id) {
 			return save(request);
 		}
 		@DeleteMapping("/{id}")
-		public @ResponseBody JsonResponse delete(@PathVariable Integer id) {
+	public @ResponseBody JsonResponse delete(@PathVariable Integer id) {
 			try {
 				Optional<Request> request = requestRepo.findById(id);
 				if(!request.isPresent()) {
@@ -68,6 +71,6 @@ public class RequestController {
 				return JsonResponse.getInstance(ex.getMessage());
 			}
 
-	    }
+	     }
  }
 
